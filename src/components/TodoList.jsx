4d9f46ebb08896730ef0129721cbdd7c143dad89
서가희 <kahee430@gmail.com>
2024-01-29 09:17:40 +0900

    style : css 수정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -22,18 +22,9 @@ const TodoList = ({ list, setList }) => {
     });
     setList(completedList);
   };
-  // select의 value에 따라 sort로 정렬하는 함수 작성
 
-  const sortFunc = (list = {
-    // 옵션 값에 따라  솔트로 데드라인으로 정렬하고 뿌리는걸 어캐할지 생각해보자 너무졸려어엉우
-  });
   return (
     <div className="todo-list-div">
-      <select className="select-box">
-        <option value="">----- todoList 정렬 옵션 -----</option>
-        <option value="asc">마감날짜 오름차순 정렬</option>
-        <option value="desc">마감날짜 내림차순 정렬</option>
-      </select>
       <h1>🔥Working</h1>
       <div className="list-div">
         {/* isDone의 상태에 따라 todo가 반환된다. */}
